3464ad6e3143286a0a41c20744f2f1aee4d6ebb9
Joelson Sartori Junior <joelsonsartori@gmail.com>
2019-03-25 17:40:49 -0300

    Create list

---
+++ list.py
@@ -0,0 +1,48 @@
+class lista:
+
+    def __init__(self, maxm):
+        self.elemento = [0]*maxm
+        self.ini = -1
+        self.fim = -1
+        self.maxm = maxm
+
+    def consultar(self, pos):
+        if pos > (self.fim - self.ini) or pos < self.ini:
+            return False
+        else:
+            return self.elemento[self.ini+pos]
+
+    def insere(self, nodo, pos):
+         #Posicao maior que o espaco alocado
+         #Posicao maior que o espaco alocado
+         #Posicao menor do que o inicio
+        if (self.ini == 0 and self.fim == self.maxm-1) or (pos > (self.fim - self.ini +1)) or  (pos < 0 or ( self.ini == -1 and pos !=0 )): 
+            print("Error: posicao invalida")
+        
+        else:
+            if self.ini == -1: #Lista vazia
+                self.ini = 0
+                self.fim = 0
+            else:
+                if self.fim != self.maxm -1:
+                    for i in range(self.fim, self.ini+ pos-1, -1):
+                        self.elemento[i+1] = self.elemento[i]
+                    self.fim = self.fim + 1
+                else:
+                    for i in range(self.ini, self.ini +pos):
+                        self.elemento[i-1] = self.elemento[i]
+                    self.ini = self.ini-1
+            self.elemento[self.ini+pos] = nodo
+
+def main():
+    l = lista(10)
+    print(l.consultar(2))
+    print(l.insere('moto', 0))
+    print(l.insere('carro', 1))
+    print(l.insere('caminhao', 5))
+    print(l.elemento)
+    print(l.insere('aviao', 0))
+    print(l.elemento)
+    
+main()
+
