c948c789f3c4b68914003b411ed7efc0ae376246
Joelson Sartori Junior <joelsonsartori@gmail.com>
2019-04-28 20:15:48 -0300

    teste

--- listasEncadeadas/linkList.py
+++ listasEncadeadas/linkList.py
@@ -7,20 +7,22 @@ class LinkedList:
         self._size = 0
 
     def append(self, elem):
-        if self.head:
+        if self.head: #Inserção quando a lista não esta vazia 
             pointer = self.head
-            while pointer.next:
+            while (pointer.next):
                 pointer = pointer.next
             pointer.next = Node(elem)
         else:
+            #Inserção primeiro elemento
             self.head = Node(elem)
+
         self._size += 1
 
     def __len__(self):
         """Retorna o tamanho da lista"""
         return self._size
 
-    def _getnode(self, index):
+    def _getNode(self, index):
         pointer = self.head
         for i in range(index):
             if pointer:
@@ -30,23 +32,26 @@ class LinkedList:
         return pointer
 
     def __getitem__(self, index):
-        pointer = self._getnode(index)
+        #Funciona a = lista[1]
+        pointer = self._getNode(index)
         if pointer:
             return pointer.data
-        raise IndexError("list index out of range")
+        else:
+            raise IndexError("list index out of range")
 
     def __setitem__(self, index, elem):
-        pointer = self._getnode(index)
+        #funciona lista[1] = 2
+        pointer = self._getNode(index)
         if pointer:
             pointer.data = elem
         else:
             raise IndexError("list index out of range")
 
     def index(self, elem):
-        """Return index element in list"""
+        """Retorna o indece do elemento na lista"""
         pointer = self.head
         i = 0
-        while pointer:
+        while (pointer):
             if pointer.data == elem:
                 return i
             pointer = pointer.next
@@ -59,19 +64,27 @@ class LinkedList:
             node.next = self.head
             self.head = node
         else:
-            pointer = self._getnode(index-1)
+            pointer = self._getNode(index - 1)
             node.next = pointer.next
             pointer.next = node
         self._size += 1
 
-
-lista = LinkedList()
-lista.append(7)
-lista.append(80)
-lista.append(56)
-lista.append(32)
-lista.append(17)
-lista.insert(0, 22)
-print(lista[0])
-lista.insert(3, 888)
-print(lista[3])
+    def remove(self, elem):
+        if self.head == None:
+            raise ValueError("{} is not in list".format(elem))
+        elif self.head.data == elem:
+            self.head = self.head.next
+            return True
+        else:
+            ancestor = self.head
+            pointer = self.head.next
+            while (pointer):
+                if pointer.data == elem:
+                    ancestor.next = pointer.next
+                    pointer.next = None
+                ancestor = ancestor.next
+                pointer = pointer.next
+            return True
+        raise ValueError("{} is not in list".format(elem))
+if __name__ == '__main__':
+    pass
---
+++ listasEncadeadas/main.py
@@ -0,0 +1,7 @@
+from linkList import LinkedList
+
+lista = LinkedList()
+
+lista.append(1)
+lista.append(2)
+len(lista)
