951e0e157dbf43caac6893ee6807cab1ffb9f8e1
Joelson Sartori Junior <joelsonsartori@gmail.com>
2019-09-29 15:22:26 -0300

    Ajustes

--- ProgramacaoDinamica/fibonacci.py
+++ ProgramacaoDinamica/fibonacci.py
@@ -1,19 +1,4 @@
-
-
-def fib_low(x):
-    """[summary]
-    
-    Arguments:
-        x {integer} -- Index Fibonacci
-    
-    Returns:
-        Integer -- Calculate number fibonacci
-    """
-    if x<= 2:
-        return 1
-    else:
-        return fib_low(x-1) + fib_low(x-2)
-
+import unittest
 
 fibobacci_cache = {}
 def fib_cache(x):
@@ -28,12 +13,22 @@ def fib_cache(x):
     if x in fibobacci_cache:
         return fibobacci_cache[x]
     
-    if x <= 2:
-        
+    if x <= 2:    
         value = 1
+
     elif x > 2:
-        
         value = fib_cache(x-1) + fib_cache(x-2)
     
     fibobacci_cache[x] = value
     return value
+
+class MyTest(unittest.TestCase):
+    def test(self):
+        self.assertEqual(fib_cache(1), 1)
+        self.assertEqual(fib_cache(2), 1)
+        self.assertEqual(fib_cache(3), 2)
+        self.assertEqual(fib_cache(4), 3)
+        self.assertEqual(fib_cache(5), 5)
+        self.assertEqual(fib_cache(6), 8)
+        self.assertEqual(fib_cache(7), 13)
+        self.assertEqual(fib_cache(8), 21)
